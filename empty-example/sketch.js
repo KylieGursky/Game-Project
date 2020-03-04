b5d02976addea7dbc8828686f6fa0ac3a343b653
KylieGursky <kyliemgursky@gmail.com>
2020-03-04 11:57:08 -0800

    Update sketch.js
    function time

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -4,13 +4,18 @@ function setup() {
 
 function draw(){
 	background(220,100,3);
-  fill("blue")
-  rect(250,200,200,150)
-  fill("yellow");
-  ellipse(330,300,100,100);
-  fill("black")
-  ellipse(300,290,50,50)
-  ellipse(360,290,50,50)
-  line(320,325,355,325)
+  face(100,100)
 
+}
+
+function face(x,y){
+  //x=250 y=200
+  fill("blue")
+  rect(x,y,x-50,y-50)
+  fill("yellow");
+  ellipse(x+80,y+100,x-150,y-100);
+  fill("black")
+  ellipse(x+250,y+90,50,50)
+  ellipse(x+110,y+90,50,50)
+  line(x+70,y+125,x+105,y+125)
 }
