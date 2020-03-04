adfb05451f178348e2fabcb0a9d076e841983607
KylieGursky <kyliemgursky@gmail.com>
2020-03-04 11:51:47 -0800

    Update sketch.js
    finishing face

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -5,11 +5,12 @@ function setup() {
 function draw(){
 	background(220,100,3);
   fill("blue")
-  rect(300,200,100,150)
+  rect(250,200,100,150)
   fill("yellow");
   ellipse(330,300,100,100);
   fill("black")
   ellipse(300,290,50,50)
   ellipse(360,290,50,50)
+  line(300,350,375,350)
 
 }
