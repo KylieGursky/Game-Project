9310a18f60c6de840432049e1db3f3400decd0b5
KylieGursky <kyliemgursky@gmail.com>
2020-03-24 13:42:15 -0700

    donut run

--- 4-sound /sketch.js
+++ 4-sound /sketch.js
@@ -1,14 +1,17 @@
 //create an empty array called balls
 let balls = [];
+let roads =[];
 
 //create a variable to hold your avatar
 let me;
 
 let mySound;
+let newSound;
 
 function preload() {
   soundFormats('mp3', 'ogg');
-  mySound = loadSound('boing1.mp3');
+  mySound = loadSound('gettinghit.mp3');
+  newSound = loadSound('gunshot3.wav');
 }
 
 function setup() {
@@ -21,22 +24,45 @@ function setup() {
 
 function draw(){
 	background(220);
+  fill("lightblue");
+  noStroke()
+  rect(0,0,500,250)
+  fill("lightgreen")
+  rect(0,250,500,250)
+  fill("gray")
+  rect(0,300,500,400)
 
   me.drawMe();
   me.moveMe();
+
+  if (frameCount % 50 == 0){
+    let b = new Road(500,350);
+    roads.push(b);
+    console.log(roads);
+  }
+    for (let i = 0; i < roads.lenght; i++){
+      roads[i].drawRoad();
+      roads[i].moveRoad();
+    }
 
   if (frameCount % 25 == 0) {
       let  b = new Ball(width, random(0,height), -3);
       balls.push(b);
       console.log(balls); //print the balls array to the console
+      // newSound.setVolume(0.5);
+      // newSound.play();
     }
-
+if (frameCount % 75 == 0){
+  newSound.setVolume(0.5);
+  newSound.play();
+}
 //	draw all the balls in that array
 	for (let i = 0; i < balls.length; i++) {
 	 	      balls[i].drawBall();
        	  balls[i].moveBall();
         	balls[i].bounceBall();
 	  }
+
 
 }
 
@@ -53,13 +79,28 @@ class Avatar {
     		stroke("green");
         strokeWeight(3);
     		fill("blue");
-		    ellipse(this.x,this.y,20,20);
+		    ellipse(this.x,this.y-10,20,20);
+        fill("black");
+        push();
+        noStroke();
+        ellipse(this.x+5,this.y-13,5,5);
+        pop();
         line(this.x,this.y, this.x, this.y+40);
         line(this.x, this.y+40, this.x-20, this.y+60);
         line(this.x, this.y+40, this.x+10, this.y+50);
         line(this.x+10, this.y+50, this.x+5, this.y+60);
         line(this.x, this.y+15, this.x-10, this.y+25);
         line(this.x-10, this.y+25, this.x+10, this.y+35);
+        noStroke();
+        fill(185,123,0);
+        ellipse(this.x+15,this.y+30,20,10);
+        fill(244,182,229);
+        ellipse(this.x+15,this.y+28,15,5);
+        fill(0);
+        ellipse(this.x+15.5,this.y+27.5,5,3)
+        rect(this.x-3,this.y+35,5,10)
+        fill("yellow")
+        rect(this.x-2.5,this.y+35.5,4,9)
 	}
 
 	moveMe(){
@@ -85,16 +126,16 @@ class Ball {
 	//every ball needs an x value, a y value, and a speed
 	constructor(x,y, speed){
 		this.x = x;
-    		this.y = y;
-        	this.speed = speed;
+    this.y = y;
+    this.speed = speed;
 	}
 
 	// draw a ball on the screen at x,y
 	drawBall(){
-    		stroke(0);
-        	strokeWeight(1);
-    		fill("red");
-		ellipse(this.x,this.y,10,10);
+    	stroke(0);
+      strokeWeight(1);
+    	fill("black");
+		  ellipse(this.x,this.y,8,5);
 	}
 
 	//update the location of the ball, so it moves across the screen
@@ -103,13 +144,37 @@ class Ball {
 		this.y = this.y+.5;
 	}
 
-	//if the ball hits the paddle, change the speed value to negative (send it in the opposite direction)
+	//if the ball hits the person, change the speed value to negative (send it in the opposite direction)
   	bounceBall(){
     		if (this.x >= me.x-15 && this.x <= me.x+15 && this.y > me.y-40 && this.y < me.y+40){
-      			this.speed = -this.speed;
-            mySound.setVolume(0.1);
+      			// this.speed = -this.speed;
+            fill(250,0,0)
+            noStroke()
+            ellipse(this.x,this.y,15,10)
+            mySound.setVolume(0.5);
             mySound.play();
     		}
   	}
+
+}
+
+class Road{
+
+  constructor(x,y, size, speed){
+		this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.size = size;
+  }
+   drawRoad(){
+     noStroke()
+     fill("yellow")
+     rect(this.x,this.y,this.size,this.size)
+   }
+
+   moveRoad(){
+     this.x = this.x+this.speed
+     this.y = this.y
+   }
 
 }
